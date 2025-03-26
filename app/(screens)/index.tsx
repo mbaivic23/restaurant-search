8ee185a5d4531fe21ca02186d794ba1ec281b12a
Mika <mikaa04@proton.me>
2025-03-26 21:30:40 +0100

    Homescreen

--- app/(screens)/index.tsx
+++ app/(screens)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-4xl text-red-500 font-bold">Home</Text>
+      <Text className="text-4xl text-banana-900 font-bold">Restaurant Finder</Text>
     </View>
   );
 }
